8c3a4b05ba58753ac1dd7c327c46ad75f8640690
JB Villegas <villegasjohnb18@gmail.com>
2025-03-31 22:07:58 +0800

    done with main and fcfs. 3 to go. Jesus loves you - 1 John 1:9

--- fcfs.py
+++ fcfs.py
@@ -1,49 +1,93 @@
-# ask user for 5 num Process
+def FCFS():    
+    #user input
+    def Process(p):
+        numbers = []    
+        print("\nEnter Process")   
+        for i in range(1, p+1):   
+            arrvl = int(input(f"Arrival Time P{i}: "))
+            burst = int(input(f"Burst Time P{i}: "))
+            print("")
+            numbers.append((f"P{i}", arrvl, burst))
+              
+        return numbers
 
+    # display    
+    def Display(pronum):
+        print("\n Process\tArrival Time\tBurst Time")
+    
+        waiting = []
+        completion = []
+        turnaround = []
+        ctime = 0
+        total_tat = 0
+        total_wt = 0
+        
+        for label, arrvl, burst in pronum:
+            ctime = max(ctime, arrvl) + burst
+            completion.append(ctime)
+            
+            tat = ctime - arrvl
+            turnaround.append(tat)
+            total_tat += tat
+            
+            wtime = ctime - arrvl - burst
+            waiting.append(wtime)
+            total_wt += wtime
+            
+            print(f" {label}\t\t{arrvl}\t\t{burst}")
+            
+        print(f"\nCompletion Time\tTurnaround Time\tWaiting Time")
+        
+        for i in range(len(pronum)):    
+            print(f" {completion[i]}\t\t{turnaround[i]}\t\t{waiting[i]}")
+        
+        atat = total_tat / len(pronum)
+        awt = total_wt / len(pronum)
+        
+        print(f"\n Average Turn around Time: {atat: .2f}\n Average waiting time: {awt: .2f}")
 
-def Process(p):
-     numbers = []    
-     print("\nEnter Process")   
-     for i in range(1, p+1):   
-        arrvl = int(input(f"Arrival Time P{i}: "))
-        burst = int(input(f"Burst Time P{i}: "))
-        print("")
-        numbers.append((f"P{i}", arrvl, burst))
+    #1st display
+    print("\nFirst Come, First Served(FCFS) ")      
+    while True:
+        try:   
+            numpt = int(input(f"How Many Process Time: ")) 
+            pronum = Process(numpt)
+            Display(pronum)
+            break
+        except ValueError:
+            print("Invalid Input, try again.\n")
+        
+def Choices(choice):
+    match choice:
+        case 1: 
+            FCFS()
+        case 2: 
+            print("SJF")
+        case 3: 
+            print("SRTF")
+        case 4: 
+            print("RR")      
+               
+def Main():
+    print("Welcome to CPU Scheduling (OS)")
+    print("Name: John Benedict M. Villegas")
+    print("Section:  BSIT 1A")
+    
+    print("\nChoose CPU Scheduling: ")
+    print(" 1. First Come, First Served (FCFS)")
+    print(" 2. Shortest Job First (SJF)")
+    print(" 3. Shortest Remaining Time First (SRTF)")
+    print(" 4. Round Robin (RR)")
+    
+    while True:
+        try:
+            choice = int(input(f"\nEnter your choice (1-4): "))
+            if choice not in [1, 2, 3, 4]:
+                raise ValueError("Invalid choice. Please enter a number between 1 and 4.") 
+            break          
+        except ValueError:
+            print("Invalid choice. Please enter a number between 1 and 4.")
+    
+    Choices(choice)   
      
-       
-     return numbers
-     
-def Display(pronum):
-    print("\n Process\tArrival Time\tBurst Time")
-
-    #input = "\n ".join(str(num) for num in numbers)+"\tsum"
-    waiting = []
-    completion = []
-    turnaround = []
-    ctime = 0
-    
-    for label, arrvl, burst in pronum:
-        ctime = max(ctime, arrvl) + burst
-        completion.append(ctime)
-        
-        wtime = ctime - arrvl - burst
-        waiting.append(wtime)
-        
-        tat = ctime - arrvl
-        turnaround.append(tat)
-        print(f" {label}\t\t{arrvl}\t\t{burst}")
-        
-    print(f"\nCompletion Time\tTurnaround Time\tWaiting Time")
-    
-    for i in range(len(pronum)):    
-        print(f" {completion[i]}\t\t{turnaround[i]}\t\t{waiting[i]}")
-    
-def Main():    
-    print("First Come, First Served(FCFS) \n")      
-    
-    numpt = int(input(f"How Many Process Time: ")) 
-    pronum = Process(numpt)
-    Display(pronum)
-
 Main()
-
